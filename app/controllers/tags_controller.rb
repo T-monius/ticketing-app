7c8c6038cc038a4a3728d5a2799968776da2a7d3
mo mac <trrl.mo@hotmail.com>
2020-05-12 09:35:18 -0600

    CRUD actions for Tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,52 @@
 class TagsController < ApplicationController
+  before_action :set_tag, only: [:edit, :update, :destroy]
 
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    if @tag = Tag.create(tag_params)
+      flash[:notice] = 'The tag was created.'
+      redirect_to tags_path
+    else
+      flash[:error] = 'Could not create tag.'
+      render :new
+    end
+  end
+
+  def edit
+    # FIXME: Edit form submits to a GET even though PATH exists
+    redirect_to tags_path
+  end
+
+  def update
+    if @tag.save
+      flash[:notice] = 'The tag was updated.'
+      redirect_to tag_path(@tag)
+    else
+      flash[:error] = 'Could not update.'
+      render :edit
+    end
+  end
+
+  def destroy
+    @tag.destroy
+
+    redirect_to tags_path
+  end
+
+  private
+
+  def set_tag
+    @tag = Tag.find(params[:id])    
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
